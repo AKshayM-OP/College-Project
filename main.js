4ebf23d7f25a8786f05057b5e3e9f5713ebdabc5
AkshayK <66427306+AKshayM-OP@users.noreply.github.com>
2024-11-20 22:24:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('openlink').addEventListener('click', function(){
+    const url = 'about.html'; 
+    window.open(url, '_blank');
+} );
+document.getElementById('openlinke').addEventListener('click', function(){
+    const url = 'content.html'; 
+    window.open(url, '_blank');
+} );
